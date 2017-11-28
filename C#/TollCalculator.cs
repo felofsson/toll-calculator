d45ff276257fab3589e477897a72e5e9447ce68c
felofsson <elofsson.fredrik@gmail.com>
2017-11-28 16:05:07 +0100

    improved the main method

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -16,28 +16,28 @@ public class TollCalculator
     public int GetTollFee(Vehicle vehicle, DateTime[] dates)
     {
         DateTime intervalStart = dates[0];
+
         int totalFee = 0;
+
+        long lastPassage = intervalStart.Millisecond;
+        totalFee += GetTollFee(intervalStart, vehicle); // Always count the first
+
+        double minutesElapsedSinceLastPassage;
+
         foreach (DateTime date in dates)
         {
-            int nextFee = GetTollFee(date, vehicle);
-            int tempFee = GetTollFee(intervalStart, vehicle);
+            minutesElapsedSinceLastPassage = (date.Millisecond - lastPassage)/(60*1000);
 
-            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            long minutes = diffInMillies/1000/60;
+            if (minutesElapsedSinceLastPassage > 60) {
+                totalFee += GetTollFee(date, vehicle);
+                lastPassage = date.Millisecond;
+            }
 
-            if (minutes <= 60)
-            {
-                if (totalFee > 0) totalFee -= tempFee;
-                if (nextFee >= tempFee) tempFee = nextFee;
-                totalFee += tempFee;
-            }
-            else
-            {
-                totalFee += nextFee;
-            }
+            if (totalFee > 60) totalFee = 60;
+
         }
-        if (totalFee > 60) totalFee = 60;
         return totalFee;
+
     }
 
     public bool IsTollFreeVehicle(Vehicle vehicle)
